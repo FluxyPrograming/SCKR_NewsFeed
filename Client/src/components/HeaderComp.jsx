1e936ffbcfe3791a0d24a035a9d5e2df5cbf8b8c
lukab <luka.blazic@gmail.com>
2024-04-05 09:38:56 +0200

    added clock still needs to update DOM

--- Client/src/components/HeaderComp.jsx
+++ Client/src/components/HeaderComp.jsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react";
+const GetTime = () => {
+  let today = new Date();
+  console.log("called");
+  return today.getHours() + ":" + today.getMinutes();
+};
 
 function HeaderComp() {
-  let today = new Date();
-  let time = today.getHours() + ":" + today.getMinutes();
-  const [CurrTime, SetCurrTime] = useState(0);
-
-  useEffect(() => {
-    return SetCurrTime(time);
-  }, [time]);
-
   return (
     <>
       <header className="bg-slate-400 w-dvw h-36 flex items-center p-10">
-        <div className="bg-green-400 w-10 h-10 ">{}</div>
+        <div className="bg-green-400 w-10 h-10 "></div>
         <div className="flex-grow"></div>
-        <div className="bg-purple-500 w-10 h-10"></div>
+        <div className="bg-purple-500 w-10 h-10">
+          <p>
+            <GetTime />
+          </p>
+        </div>
       </header>
     </>
   );
 }
 
+setInterval(GetTime, 1000);
 export default HeaderComp;
